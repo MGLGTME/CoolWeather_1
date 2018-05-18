3321e15913cf3540789cc790463c993b68e769b2
GTME <479616729@qq.com>
2018-05-18 09:04:56 +0800

    first commit.

---
+++ app/src/main/java/com/example/gtme/coolweather_1/gson/Now.java
@@ -0,0 +1,14 @@
+package com.example.gtme.coolweather_1.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperature;
+    @SerializedName("cond")
+    public More more;
+    public class More{
+        @SerializedName("tet")
+        public String info;
+    }
+}
